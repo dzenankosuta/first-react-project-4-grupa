29dfa83c0d9fcefce5eaa159eb7e1af5de7c4e36
Dzenan Košuta <dzenan_kosuta@hotmail.com>
2024-05-27 21:03:01 +0200

    hotel card new styles

--- src/components/Cards/HotelCard/HotelCard.jsx
+++ src/components/Cards/HotelCard/HotelCard.jsx
@@ -5,7 +5,7 @@ import { LiaHotelSolid } from "react-icons/lia";
 function HotelCard(props) {
   return (
     <div className="card">
-      <img src={props?.imageUrl} width={270} />
+      <img src={props?.imageUrl} width={270} height={170} />
       <div className="card-title">{props?.title}</div>
       <div className="card-content">{props?.content}</div>
       <div className="card-text">Per Night</div>
