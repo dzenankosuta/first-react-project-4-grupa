c9884f1d800b818e23f23e1f840ced1465ca77d3
Dzenan Košuta <dzenan_kosuta@hotmail.com>
2024-06-03 21:45:12 +0200

    get hotel id - use Params

--- src/pages/Hotel/Hotel.jsx
+++ src/pages/Hotel/Hotel.jsx
@@ -1,7 +1,23 @@
+import { useParams } from "react-router-dom";
 import "./Hotel.css";
+import hotels from "../../common/hotels.json";
 
 function Hotel() {
-  return <div>Hotel</div>;
+  const { id } = useParams();
+  console.log(hotels);
+
+  return <div>Hotel sa id-jem {id}</div>;
 }
 
 export default Hotel;
+
+// Domaci zadatak:
+
+// Obnoviti sledece metode kod nizova:
+
+// forEach()
+// map()
+// filter()
+// reduce()
+// find()
+// findIndex()
