173673aaca3a84d1e784df1749a3ef1e91db9ba2
Dzenan Košuta <dzenan_kosuta@hotmail.com>
2024-06-10 21:16:43 +0200

    updated Teams

--- src/pages/Teams/Teams.jsx
+++ src/pages/Teams/Teams.jsx
@@ -1,22 +1,45 @@
 /* eslint-disable react/jsx-key */
-import teams from "../../common/teams.json";
+import { useEffect, useState } from "react";
+import teamsJSON from "../../common/teams.json";
 import TeamCard from "../../components/Cards/TeamCard/TeamCard";
 import "./Teams.css";
 
 export default function Teams() {
+  const [teams, setTeams] = useState(teamsJSON);
+
+  // console.log(Math.random(0, 1));
+  // console.log(new Date().getTime());
+
+  const deleteTeam = (id) => {
+    const filteredTeams = teams.filter((team) => team.id !== id);
+    setTeams(filteredTeams);
+  };
+
+  useEffect(() => {
+    const newTeams = teamsJSON.map((team) => {
+      const teamId = Math.random(0, 100000000000);
+      return {
+        ...team,
+        id: teamId,
+      };
+    });
+    setTeams(newTeams);
+  }, []);
   return (
     <>
       <div className="list">
-        {teams.map((value) => {
+        {teams.map((team, index) => {
           return (
             <TeamCard
-              name={value["team_name"]}
-              points={`${value["points"]}pts`}
-              wins={value.wins}
-              loses={value.loses}
-              draws={value.draws}
-              losses={value.losses}
-              matches={value.matches_played}
+              key={index}
+              name={team["team_name"]}
+              points={`${team["points"]}pts`}
+              wins={team.wins}
+              loses={team.loses}
+              draws={team.draws}
+              losses={team.losses}
+              matches={team.matches_played}
+              deleteTeam={() => deleteTeam(team.id)}
             />
           );
         })}
