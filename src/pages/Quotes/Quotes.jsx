ce4b0ff97add353b7696478520c4f2015d585b27
Dzenan Košuta <dzenan_kosuta@hotmail.com>
2024-06-24 20:43:56 +0200

    add mui pagination

--- src/pages/Quotes/Quotes.jsx
+++ src/pages/Quotes/Quotes.jsx
@@ -4,11 +4,16 @@ import QuoteCard from "../../components/Cards/QuoteCard/QuoteCard";
 import CircularProgress from "@mui/material/CircularProgress";
 import Box from "@mui/material/Box";
 import "./Quotes.css";
+import Pagination from "@mui/material/Pagination";
 
 function Quotes() {
   const [quotes, setQuotes] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [page, setPage] = useState(12);
+  const [page, setPage] = useState(1);
+  const handleChange = (event, value) => {
+    setPage(value);
+  };
+  const [totalPages, setTotalPages] = useState(0);
 
   async function getQuotes() {
     setIsLoading(true);
@@ -19,14 +24,18 @@ function Quotes() {
       const result = await response.json();
       console.log(result);
       setQuotes(result.results);
+      setTotalPages(result.totalPages);
     } catch (error) {
       console.log(error);
     } finally {
       setIsLoading(false);
     }
   }
-
   useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
     getQuotes();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [page]);
@@ -37,14 +46,25 @@ function Quotes() {
           <CircularProgress />
         </Box>
       ) : (
-        quotes.map((quote) => (
-          <QuoteCard
-            key={quote._id}
-            author={quote.author}
-            quote={quote.content}
-            tags={quote.tags}
-          />
-        ))
+        <>
+          {quotes.map((quote) => (
+            <QuoteCard
+              key={quote._id}
+              author={quote.author}
+              quote={quote.content}
+              tags={quote.tags}
+            />
+          ))}
+          <div className="pagination-wrapper">
+            <Pagination
+              count={totalPages}
+              page={page}
+              onChange={handleChange}
+              size="large"
+              color="primary"
+            />
+          </div>
+        </>
       )}
     </div>
   );
